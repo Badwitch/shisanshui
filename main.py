fe94701d2ea07070ba20a3caaf25a825643cdde2
Badwitch <36150059+Badwitch@users.noreply.github.com>
2019-10-30 22:16:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import time
 import json
-import dd as shisanshui
+import sss as shisanshui
 import sys
 
 shisanshui.login()#登录
 
-try:
 
-    timestart = time.perf_counter
-    shisanshui.init_start()
-    shisanshui.read_opengame()
-    shisanshui.dfs_1(1, 1)
-    shisanshui.submit_ans = shisanshui.printf_ans()
-    shisanshui.submitgame(shisanshui.submit_ans)  #提交牌局
-    shisanshui.ranking()#显示排行榜
-    timeend = time.perf_counter
-    #print(str(timeend - timestart))
-    shisanshui.init_end()
-except IndexError:
-   time().sleep(5)
+#timestart = time.perf_counter
+shisanshui.init_start()
+shisanshui.read_opengame()
+shisanshui.dfs_1(1, 1)
+shisanshui.submit_ans = shisanshui.printf_ans()
+shisanshui.submitgame(shisanshui.submit_ans)  #提交牌局
+shisanshui.ranking()#显示排行榜
+#timeend = time.perf_counter
+#print(str(timeend - timestart))
+shisanshui.init_end()
+
